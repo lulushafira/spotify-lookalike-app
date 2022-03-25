b08f22d5ae08c3a78e6bc54d2d8914d8398212f5
Lulu Shafira Aulia <you@example.com>
2022-03-26 01:16:07 +0700

    Listing songs with map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import './App.css';
-import Card from './components/Card';
-import data from './data/sample';
+import React from "react";
+import "./App.css";
+import Card from "./components/Card";
+import data from "./data/sample";
 
 function App() {
   return (
-    <div class="container">
-      <Card
-        album__image={data.album.images[1].url}
-        album__name={data.album.name}
-        title={data.name}
-        artists={data.artists[0].name}
-      />
+    <div className="container">
+      <div className="cards">
+        {data.map((e) => (
+          <Card
+            album__image={e.album.images[1].url}
+            album__name={e.album.name}
+            title={e.name}
+            artists={e.artists[0].name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
-function Card({album__image, album__name, title, artists}) {
+function Card({ album__image, album__name, title, artists }) {
   return (
     <div class="card-wrap">
-      <div class="content">
-        <img class="img-album" src={album__image} alt={album__name}/>
-        <h3>{title}</h3>
-        <p>{artists}</p>    
-        <input class="btn "type="button" value="Select" />
+      <div className="content">
+        <img className="img-album" src={album__image} alt={album__name} />
+        <div className="info">
+          <h3>{title}</h3>
+          <p>{artists}</p>
+        </div>
+        <input className="btn " type="button" value="Select" />
       </div>
     </div>
   );
