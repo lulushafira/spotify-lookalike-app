359d51605e0782eb27b4100d81cdb5249a0f3040
lulushafira <92662721+lulushafira@users.noreply.github.com>
2022-04-22 17:12:05 +0700

    Update host

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
-const getHost: () =>  string = () => {
-  const protocol: string = window.location.protocol;
-  const host: string = window.location.host;
+const getHost = () => {
+  const protocol = window.location.protocol;
+  const host = window.location.host;
 
   return `${protocol}//${host}`;
 }
@@ -8,6 +8,7 @@ const getHost: () =>  string = () => {
 const config = {
   SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
   SPOTIFY_SCOPE: 'playlist-modify-private',
+  HOST: getHost(),
 }
 
 export default config;
